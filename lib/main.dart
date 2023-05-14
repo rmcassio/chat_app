395d39855bc3bf17b78a747fcaffc09e6abe1e08
rmcassio <cassio_r.m@hotmail.com>
2023-05-14 09:45:04 -0300

    adicionado firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:chat_app/core/services/notification/chat_notification_service.dart';
 import 'package:chat_app/pages/auth_or_app_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 void main() {
+  Firebase.initializeApp();
   runApp(const MyApp());
 }
 
